59e10c43aba09ed0b0c6fb9f9f74fa9faf73ef2f
Pratap Singh <kdarious@gmail.com>
2016-10-20 00:43:30 +0530

    Update VPC_ID.groovy
    VPC_ID.groovy

--- Jenkins-Job-To-Launch-Ec2-Instances/VPC_ID.groovy
+++ Jenkins-Job-To-Launch-Ec2-Instances/VPC_ID.groovy
@@ -1,5 +1,5 @@
 if (Select_Region != "None"){
-    def cmd1="/opt/scripts/Instance_Creation/get_vpc.sh $Select_Region $Select_Account"
+    def cmd1="/bin/bash /opt/scripts/Instance_Creation/get_vpc.sh $Select_Region $Select_Account"
     def b = new StringBuffer()
     def proc = cmd1.execute()
     proc.consumeProcessErrorStream(b)

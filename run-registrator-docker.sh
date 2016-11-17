0990e1543f323d3f9c453f9ba67cdc150d9919bd
Pratap Singh <kdarious@gmail.com>
2016-11-17 23:44:39 +0530

    Create run-registrator-docker.sh

---
+++ run-registrator-docker.sh
@@ -0,0 +1,46 @@
+Content-Type: multipart/mixed; boundary="==BOUNDARY=="
+MIME-Version: 1.0
+
+--==BOUNDARY==
+MIME-Version: 1.0
+Content-Type: text/text/x-shellscript; charset="us-ascii"
+
+#!/bin/bash
+# Specify the cluster that the container instance should register into
+cluster=your_cluster_name
+
+# Write the cluster configuration variable to the ecs.config file
+# (add any other configuration variables here also)
+echo ECS_CLUSTER=$cluster >> /etc/ecs/ecs.config
+
+# Install the AWS CLI and the jq JSON parser
+yum install -y aws-cli jq
+--==BOUNDARY==
+MIME-Version: 1.0
+Content-Type: text/text/upstart-job; charset="us-ascii"
+
+#upstart-job
+description "Amazon EC2 Container Service (start task on instance boot)"
+author "Amazon Web Services"
+start on started ecs
+
+script
+	exec 2>>/var/log/ecs/ecs-start-task.log
+	set -x
+	until curl -s http://localhost:51678/v1/metadata
+	do
+		sleep 1
+	done
+
+	# Grab the container instance ARN and AWS region from instance metadata
+	instance_arn=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F/ '{print $NF}' )
+	cluster=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .Cluster' | awk -F/ '{print $NF}' )
+	region=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F: '{print $4}')
+
+	# Specify the task definition to run at launch
+	task_definition=my_task_def
+
+	# Run the AWS CLI start-task command to start your task on this container instance
+	aws ecs start-task --cluster $cluster --task-definition $task_definition --container-instances $instance_arn --started-by $instance_arn --region $region
+end script
+--==BOUNDARY==--

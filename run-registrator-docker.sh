08a2d1597b0da709bf689f48b9bd57e5d8ba780e
Pratap Singh <kdarious@gmail.com>
2016-11-17 23:46:22 +0530

    Update run-registrator-docker.sh

--- run-registrator-docker.sh
+++ run-registrator-docker.sh
@@ -7,11 +7,11 @@ Content-Type: text/text/x-shellscript; charset="us-ascii"
 
 #!/bin/bash
 # Specify the cluster that the container instance should register into
-cluster=your_cluster_name
+cluster=$1
 
 # Write the cluster configuration variable to the ecs.config file
 # (add any other configuration variables here also)
-echo ECS_CLUSTER=$cluster >> /etc/ecs/ecs.config
+#echo ECS_CLUSTER=$cluster >> /etc/ecs/ecs.config
 
 # Install the AWS CLI and the jq JSON parser
 yum install -y aws-cli jq
@@ -38,7 +38,7 @@ script
 	region=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F: '{print $4}')
 
 	# Specify the task definition to run at launch
-	task_definition=my_task_def
+	task_definition=$2
 
 	# Run the AWS CLI start-task command to start your task on this container instance
 	aws ecs start-task --cluster $cluster --task-definition $task_definition --container-instances $instance_arn --started-by $instance_arn --region $region

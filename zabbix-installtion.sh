83a740c65b634517fad4882a3fde2f3b1ad40015
Pratap Singh <kdarious@gmail.com>
2017-04-30 00:34:54 +0530

    Update zabbix-installtion.sh

--- zabbix-installtion.sh
+++ zabbix-installtion.sh
@@ -7,6 +7,7 @@
 Hostname=${1}
 Hostmetadata=${2}
 SERVER=${3}  #zabbix server ip or fqdn name
+StartAgents=${4}
 
 if which lsb_release &>> /dev/null; then
     OS=`lsb_release -a | sed -n 's/.*\(Ubuntu\).*/\1/p; s/.*\(Amazon\).*/\1/p; s/.*\(Debian\).*/\1/p' | head -1`
@@ -24,17 +25,27 @@ echo -e "\n=====================================================================
 case "$OS" in
 "Ubuntu")
         URL='http://repo.zabbix.com/zabbix/3.2/ubuntu/pool/main/z/zabbix/zabbix-agent_3.2.0-1+trusty_amd64.deb'
-        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb ; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'` ; HOSTNAME=${Hostname} ; sed -i "s/Hostname=.*/Hostname=${HOSTNAME}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=0' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
+        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb ; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'` ; HOSTNAME=${Hostname} ; sed -i "s/Hostname=.*/Hostname=${HOSTNAME}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=${StartAgents}' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
 ;;
 
 "Debian")
         URL='http://repo.zabbix.com/zabbix/3.2/debian/pool/main/z/zabbix/zabbix-agent_3.2.0-1+jessie_amd64.deb'
-        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=$Hostmetadata" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=0' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ;/etc/init.d/zabbix-agent start; rm /tmp/zabbix-agent*; /etc/init.d/zabbix-agent restart
+        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=$Hostmetadata" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=${StartAgents}' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ;/etc/init.d/zabbix-agent start; rm /tmp/zabbix-agent*; /etc/init.d/zabbix-agent restart
 ;;
 
 "Amazon")
         URL='http://repo.zabbix.com/zabbix/3.2/rhel/6/x86_64/zabbix-agent-3.2.0-1.el6.x86_64.rpm'
 
-        yum update -y ; yum install unixODBC.x86_64  -y ; wget $URL; rpm -ivh zabbix-agent-*.rpm; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'|grep -v eth` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=0' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
+        yum update -y ; yum install unixODBC.x86_64  -y ; wget $URL; rpm -ivh zabbix-agent-*.rpm; rand=`ip r| grep -Ev "default|linkdown" |  awk '{print $NF}'|grep -v eth` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i '/StartAgents=3/aStartAgents=${StartAgents}' /etc/zabbix/zabbix_agentd.conf; sed -i '/# ListenIP=0.0.0.0/a ListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf ; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
 
 esac
+
+# diskstats user parameters config
+sudo mkdir -p /etc/zabbix/zabbix_agentd.d/
+sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/userparameter_diskstats.conf -O /etc/zabbix/zabbix_agentd.d/userparameter_diskstats.conf
+
+# low level discovery script
+sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/lld-disks.py -O /usr/local/bin/lld-disks.py
+sudo chmod +x /usr/local/bin/lld-disks.py
+
+sudo service zabbix-agent restart

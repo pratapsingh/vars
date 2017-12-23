120e8abfbfa3d52201bb1bf9533e21802b3f985f
Pratap Singh <kdarious@gmail.com>
2017-12-23 18:00:50 +0530

    Update install-zabbix-on-public-server.sh

--- install-zabbix-on-public-server.sh
+++ install-zabbix-on-public-server.sh
@@ -8,54 +8,58 @@ Hostname=${1}
 Hostmetadata=${2}
 SERVER=${3}  #zabbix server ip or fqdn name
 SA=${4}
-
-if [ "${SA}" -gt 0 ] ; then
-  #IP=`hostname -I | awk {'print $1'}`
-  #IP=`ip r |grep "dev eth0"| tail -1 | awk {'print $NF'}`
-  IP=`curl wgetip.com`
+if [[ ! -z `dpkg -l | grep -i zabbix` ]] ;then
+  echo -e "\nZabbix agent already installed\n";
 else
-  IP='0.0.0.0'
-fi
 
-if which lsb_release &>> /dev/null; then
+  if [[ "${SA}" -gt 0 ]] ; then
+    #IP=`hostname -I | awk {'print $1'}`
+    #IP=`ip r |grep "dev eth0"| tail -1 | awk {'print $NF'}`
+    IP=`curl wgetip.com`
+  else
+    IP='0.0.0.0'
+  fi
+
+  if which lsb_release &>> /dev/null; then
     OS=`lsb_release -a | sed -n 's/.*\(Ubuntu\).*/\1/p; s/.*\(Amazon\).*/\1/p; s/.*\(Debian\).*/\1/p' | head -1`
-elif [ -r /etc/issue ]; then
+  elif [[ -r /etc/issue ]]; then
     OS=`cat /etc/issue | head -1 | awk {'print $1'}`
-else
+  else
     echo "Oops!! Unknown OS"
-fi
+  fi
 
 
-echo -e"Operation System ===> $OS\n"
-echo -e "\nBegning Zabbix agent installation on OS -> $OS on host `hostname -I`"
-echo -e "\n============================================================================================\n"
+  echo -e"Operation System ===> $OS\n"
+  echo -e "\nBegning Zabbix agent installation on OS -> $OS on host `hostname -I`"
+  echo -e "\n============================================================================================\n"
 
-case "$OS" in
-"Ubuntu")
+  case "$OS" in
+  "Ubuntu")
         URL='http://repo.zabbix.com/zabbix/3.2/ubuntu/pool/main/z/zabbix/zabbix-agent_3.2.0-1+trusty_amd64.deb'
         cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb ; rand=`hostname -I` ; HOSTNAME=${Hostname} ; sed -i "s/Hostname=.*/Hostname=${HOSTNAME}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf;  /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
-;;
+  ;;
 
-"Debian")
+  "Debian")
         URL='http://repo.zabbix.com/zabbix/3.2/debian/pool/main/z/zabbix/zabbix-agent_3.2.0-1+jessie_amd64.deb'
         cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=$Hostmetadata" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ;sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent start; rm /tmp/zabbix-agent*; /etc/init.d/zabbix-agent restart
-;;
+  ;;
 
-"Amazon")
+  "Amazon")
         URL='http://repo.zabbix.com/zabbix/3.2/rhel/6/x86_64/zabbix-agent-3.2.0-1.el6.x86_64.rpm'
 
         yum update -y ; yum install unixODBC.x86_64  -y ; wget $URL; rpm -ivh zabbix-agent-*.rpm; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
 
-esac
+  esac
 
-sleep 10
+  sleep 10
 
-# diskstats user parameters config
-sudo mkdir -p /etc/zabbix/zabbix_agentd.d/
-sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/userparameter_diskstats.conf -O /etc/zabbix/zabbix_agentd.d/userparameter_diskstats.conf
+  # diskstats user parameters config
+  sudo mkdir -p /etc/zabbix/zabbix_agentd.d/
+  sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/userparameter_diskstats.conf -O /etc/zabbix/zabbix_agentd.d/userparameter_diskstats.conf
 
-# low level discovery script
-sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/lld-disks.py -O /usr/local/bin/lld-disks.py
-sudo chmod +x /usr/local/bin/lld-disks.py
+  # low level discovery script
+  sudo wget https://raw.githubusercontent.com/grundic/zabbix-disk-performance/master/lld-disks.py -O /usr/local/bin/lld-disks.py
+  sudo chmod +x /usr/local/bin/lld-disks.py
 
-sudo service zabbix-agent restart
+  sudo service zabbix-agent restart
+fi

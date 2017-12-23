1d50a008cba8a01db92d9a1b377bf71aa90fe117
Pratap Singh <kdarious@gmail.com>
2017-12-23 19:04:29 +0530

    Update install-zabbix-on-public-server.sh

--- install-zabbix-on-public-server.sh
+++ install-zabbix-on-public-server.sh
@@ -10,7 +10,7 @@ SERVER=${3}  #zabbix server ip or fqdn name
 SA=${4}
 if [[ ! -z `dpkg -l | grep -i zabbix` ]] ;then
   echo -e "\nZabbix agent already installed\n";
-  sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf;
+  sudo sed -i "/# EnableRemoteCommands=0/a EnableRemoteCommands=1" /etc/zabbix/zabbix_agentd.conf;
   sudo service zabbix-agent restart
   
 else
@@ -39,18 +39,18 @@ else
   case "$OS" in
   "Ubuntu")
         URL='http://repo.zabbix.com/zabbix/3.2/ubuntu/pool/main/z/zabbix/zabbix-agent_3.2.0-1+trusty_amd64.deb'
-        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb ; rand=`hostname -I` ; HOSTNAME=${Hostname} ; sed -i "s/Hostname=.*/Hostname=${HOSTNAME}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf;  /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
+        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb ; rand=`hostname -I` ; HOSTNAME=${Hostname} ; sed -i "s/Hostname=.*/Hostname=${HOSTNAME}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/a EnableRemoteCommands=1" /etc/zabbix/zabbix_agentd.conf;  /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
   ;;
 
   "Debian")
         URL='http://repo.zabbix.com/zabbix/3.2/debian/pool/main/z/zabbix/zabbix-agent_3.2.0-1+jessie_amd64.deb'
-        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=$Hostmetadata" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ;sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent start; rm /tmp/zabbix-agent*; /etc/init.d/zabbix-agent restart
+        cd /tmp; apt-get  update; apt-get install wget telnet -y ; apt-get install lsb-core libodbc1 libltdl7  libcurl3 -y ; wget $URL ; dpkg -i zabbix-agent_*.deb; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=$Hostmetadata" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ;sed -i "/# EnableRemoteCommands=0/a EnableRemoteCommands=1" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent start; rm /tmp/zabbix-agent*; /etc/init.d/zabbix-agent restart
   ;;
 
   "Amazon")
         URL='http://repo.zabbix.com/zabbix/3.2/rhel/6/x86_64/zabbix-agent-3.2.0-1.el6.x86_64.rpm'
 
-        yum update -y ; yum install unixODBC.x86_64  -y ; wget $URL; rpm -ivh zabbix-agent-*.rpm; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
+        yum update -y ; yum install unixODBC.x86_64  -y ; wget $URL; rpm -ivh zabbix-agent-*.rpm; rand=`hostname -I` ; sed -i "s/Hostname=.*/Hostname=${Hostname}-${rand}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/Server=127.0.0.1/Server=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "s/ServerActive=127.0.0.1/ServerActive=${SERVER}/g" /etc/zabbix/zabbix_agentd.conf; sed -i "/# HostMetadata=/aHostMetadata=${Hostmetadata}" /etc/zabbix/zabbix_agentd.conf; sed -i "/StartAgents=3/aStartAgents=${SA}" /etc/zabbix/zabbix_agentd.conf; sed -i "/# ListenIP=0.0.0.0/a ListenIP=${IP}" /etc/zabbix/zabbix_agentd.conf ; sed -i "/# EnableRemoteCommands=0/a EnableRemoteCommands=1" /etc/zabbix/zabbix_agentd.conf; /etc/init.d/zabbix-agent restart; rm /tmp/zabbix-agent*;
 
   esac
 
